2e94ea3db66c0ce15b4769136232c4320a5542f3
Purplesmaug96 <purplesmaug96@protonmail.com>
2026-05-07 18:07:28 +0800

    Add some more things and WSAStartup

--- source/include/win/winsock2.h
+++ source/include/win/winsock2.h
@@ -1,7 +1,28 @@
 #pragma once
 
+#include <stdint.h>
+#include <stdio.h>
+
 #include <ws2def.h>
 
 typedef int SOCKET;
 
 typedef struct {} WSADATA;
+typedef WSADATA* LPWSADATA;
+
+#define AF_INET 0 // address family for IPv4
+
+enum {
+	SOCK_STREAM=0, // Supports reliable connection-oriented byte stream communication.
+	SOCK_DGRAM, // Supports unreliable connectionless datagram communication.
+	SOCK_RAW // Supports raw access to the transport protocol.
+};
+
+enum {
+	SOCKET_ERROR=0
+};
+
+int WSAStartup(/*WORD*/ uint16_t wVersionRequired, LPWSADATA lpWSAData) {
+	printf("Stubbed function WSAStartup called\n");
+	return SOCKET_ERROR;
+}
