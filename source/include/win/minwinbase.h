18643d55462c7cda562b62bf631520a0ac62aef3
Purplesmaug96 <purplesmaug96@protonmail.com>
2026-05-10 12:03:11 +0800

    Implement GetSystemTime, GetLocalTime

--- source/include/win/minwinbase.h
+++ source/include/win/minwinbase.h
@@ -1,3 +1,16 @@
 #pragma once
 
+#include <windef.h>
+
 typedef struct {} OVERLAPPED;
+
+typedef struct _SYSTEMTIME {
+	WORD wYear;
+	WORD wMonth;
+	WORD wDayOfWeek;
+	WORD wDay;
+	WORD wHour;
+	WORD wMinute;
+	WORD wSecond;
+	WORD wMilliseconds;
+} SYSTEMTIME;
--- source/include/win/sysinfoapi.h
+++ source/include/win/sysinfoapi.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <time.h>
+
 #include <windef.h>
+#include <minwinbase.h>
 
 typedef struct {
 	DWORD dwOSVersionInfoSize;
@@ -12,3 +15,23 @@ static inline BOOL GetVersionExA(OSVERSIONINFOA* verInfo) {
 	verInfo->dwPlatformId = VER_PLATFORM_WIN32_NT;
 	return TRUE;
 }
+
+static inline void GetSystemTime(SYSTEMTIME* lpSystemTime) {
+	if (lpSystemTime == NULL) {return;}
+	tm time;
+	asctime(&time);
+
+	lpSystemTime->wYear = time.tm_year;
+	lpSystemTime->wMonth = time.tm_mon;
+	lpSystemTime->wDay = time.tm_mday; // might be yday
+	lpSystemTime->wDayOfWeek = time.tm_wday;
+	lpSystemTime->wHour = time.tm_hour;
+	lpSystemTime->wMinute = time.tm_min;
+	lpSystemTime->wSecond = time.tm_sec;
+	lpSystemTime->wMilliseconds = time.tm_sec / 1000.0f;
+}
+
+static inline void GetLocalTime(SYSTEMTIME* lpSystemTime) {
+	return GetSystemTime(lpSystemTime);
+}
+
