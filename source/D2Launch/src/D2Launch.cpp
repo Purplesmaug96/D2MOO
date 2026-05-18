5d59a69c701ab7e8d4e294833e065b2f15caefad
Purplesmaug96 <purplesmaug96@protonmail.com>
2026-05-18 21:15:00 +0800

    Modify D2Launch stub to return D2Client

--- source/D2Launch/src/D2Launch.cpp
+++ source/D2Launch/src/D2Launch.cpp
@@ -1,11 +1,26 @@
 #include "D2Launch.h"
 
+#include "D2CommonDefinitions.h"
 #include <stddef.h>
+
+// Should be the same as in Main.h
+enum D2_MODULES {
+	MODULE_NONE,
+	MODULE_CLIENT,
+	MODULE_SERVER,
+	MODULE_MULTIPLAYER,
+	MODULE_LAUNCHER,
+#if D2_HAS_MULTILAN
+	MODULE_MULTILAN,
+#endif
+	MODULE_EXPAND,
+	D2_MODULES_COUNT
+};
 
 extern "C" {
 
 typedef void*(__stdcall* ModuleInitPointer)(void*);
-void* __stdcall UnkFunc(void*) { return NULL; }
+void* __stdcall UnkFunc(void*) { return (void*)MODULE_CLIENT; }
 
 // A global variable to hold the actual function pointer
 static ModuleInitPointer gInterface = UnkFunc;
