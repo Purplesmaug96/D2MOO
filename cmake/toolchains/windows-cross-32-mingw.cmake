e145cc0e907cb64cb39f15c9affed1bfcb850d1d
Purplesmaug96 <purplesmaug96@protonmail.com>
2026-06-08 17:05:57 +0800

    Changes for mingw (doesnt work...)

--- cmake/toolchains/windows-cross-32-mingw.cmake
+++ cmake/toolchains/windows-cross-32-mingw.cmake
@@ -3,10 +3,20 @@
 # Retrieved 2026-05-05, License - CC BY-SA 4.0
 
 # the name of the target operating system
-set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 set(CMAKE_C_FLAGS "-m32")
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 set(CMAKE_CXX_FLAGS "-m32")
+
+# Force MinGW headers to be used
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -isystem /usr/i686-w64-mingw32/include")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -isystem /usr/i686-w64-mingw32/include")
+
+# Prevent system headers from interfering
+set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
--- cmake/toolchains/windows-cross-64-mingw.cmake
+++ cmake/toolchains/windows-cross-64-mingw.cmake
@@ -3,7 +3,7 @@
 # Retrieved 2026-05-05, License - CC BY-SA 4.0
 
 # the name of the target operating system
-set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
