855e442cb477bd1f5ba6d7282ae07250c8422090
Purplesmaug96 <purplesmaug96@protonmail.com>
2026-08-05 15:37:13 +0800

    Use raw MessageBoxA on windows if SDL hasnt inited yet

--- source/Fog/src/ErrorManager.cpp
+++ source/Fog/src/ErrorManager.cpp
@@ -2,7 +2,22 @@
 
 #include <stdio.h>
 
-#include <SDL2/SDL_messagebox.h>
+#include <SDL2/SDL.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+static void showMessageBox(const char* title, const char* desc, bool isError) {
+	#ifdef _WIN32
+	if (!SDL_WasInit(SDL_INIT_VIDEO)) {
+		MessageBoxA(NULL, title, desc, MB_OK);
+	} else
+	#endif
+	{
+		SDL_ShowSimpleMessageBox(isError ? SDL_MESSAGEBOX_ERROR : SDL_MESSAGEBOX_WARNING, title, desc, NULL);
+	}
+}
 
 // Fog.#10023
 void FOG_DisplayAssert(const char* szMsg, const char* szFile, int nLine) {
@@ -12,7 +27,7 @@ void FOG_DisplayAssert(const char* szMsg, const char* szFile, int nLine) {
 	sprintf(tmp, "\n\nFailed at %s:%d", szFile, nLine);
 	strcat(msg, tmp);
 
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Diablo II: Assertion Failed", msg, NULL);
+	showMessageBox("Diablo II: Assertion Failed", msg, true);
 }
 
 // Fog.#10024
@@ -23,7 +38,7 @@ void FOG_DisplayHalt(const char* szMsg, const char* szFile, int nLine) {
 	sprintf(tmp, "\n\nFailed at %s:%d", szFile, nLine);
 	strcat(msg, tmp);
 
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Diablo II: Halt", msg, NULL);
+	showMessageBox("Diablo II: Halt", msg, true);
 }
 
 // Fog.#10025
@@ -34,7 +49,7 @@ void FOG_DisplayWarning(const char* szMsg, const char* szFile, int nLine) {
 	sprintf(tmp, "\n\nWarned at %s:%d", szFile, nLine);
 	strcat(msg, tmp);
 
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_WARNING, "Diablo II: Warning", msg, NULL);
+	showMessageBox("Diablo II: Warning", msg, false);
 }
 
 // Fog.#10026
@@ -45,5 +60,5 @@ void FOG_DisplayError(int nCategory, const char* szMsg, const char* szFile, int 
 	sprintf(tmp, "\n\nFailed at %s:%d\nError category: %d", szFile, nLine, nCategory);
 	strcat(msg, tmp);
 
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Diablo II: Error", msg, NULL);
+	showMessageBox("Diablo II: Error", msg, true);
 }
